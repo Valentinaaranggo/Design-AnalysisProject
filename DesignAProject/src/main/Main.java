c5c09a6e72e171b5a72531d656d8bf3c8513561b
valentinaaranggo <valea28@gmail.com>
2023-11-30 18:50:44 -0500

    made the matrix symmetric and implemented a method that create three graph matrices and the num of location is picked randomly from a 3-5

--- DesignAProject/src/main/Main.java
+++ DesignAProject/src/main/Main.java
@@ -1,36 +1,46 @@
 package main;
 
 import java.util.Arrays;
-//import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        // Generate synthetic data for a complete graph
-        int numLocations = 3;
-        generateData(numLocations);
+        generateMultipleGraphs(3);
 
     }
 
-    private static int[][] generateData(int numLocations) {
+    public static int[][] generateData(int numLocations) {
         int[][] syntheticGraph = new int[numLocations][numLocations];
         Random random = new Random();
 
         for (int i = 0; i < numLocations; i++) {
             for (int j = 0; j < numLocations; j++) {
-                if (i != j) {
-                    syntheticGraph[i][j] = random.nextInt(10) + 1; // Random weight between 1 and 10
-                } else {
+                int weight = random.nextInt(10) + 1;
+                if (i == j) {
                     syntheticGraph[i][j] = 0;
+                } else {
+                    syntheticGraph[i][j] = weight;
+                    syntheticGraph[j][i] = weight;
                 }
             }
         }
+
         System.out.println("Synthetic Graph Matrix:");
         for (int[] row : syntheticGraph) {
             System.out.println(Arrays.toString(row));
         }
 
         return syntheticGraph;
+    }
+
+    public static void generateMultipleGraphs(int numGraphs) {
+
+        for (int i = 1; i <= numGraphs; i++) {
+            Random random = new Random();
+            int numLocations = random.nextInt(4) + 3;
+            generateData(numLocations);
+        }
+
     }
 
 }
